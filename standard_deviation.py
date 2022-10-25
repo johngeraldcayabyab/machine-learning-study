f5b0b97f6d3342adc5d0c64842fdaf4eec01f092
Dlareg <johngeraldcayabyab@gmail.com>
2022-10-26 04:34:20 +0800

    generate normal distribution data

--- standard_deviation.py
+++ standard_deviation.py
@@ -1,5 +1,8 @@
 import numpy
 
+# Best explanation of standard deviation formula
+# https://www.khanacademy.org/math/statistics-probability/summarizing-quantitative-data/variance-standard-deviation-population/a/calculating-standard-deviation-step-by-step
+
 print(numpy.std([86, 87, 88, 86, 87, 85, 86]))
 
 print(numpy.std([32, 111, 138, 28, 59, 77, 97]))
