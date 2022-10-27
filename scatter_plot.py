82b4d3e7c24f692be698b9dca5ba2eb81a06c3e4
Dlareg <johngeraldcayabyab@gmail.com>
2022-10-27 11:12:46 +0800

    testing how to make scatter plot with matplot

---
+++ scatter_plot.py
@@ -0,0 +1,14 @@
+import numpy
+import matplotlib.pyplot as plt
+
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+#plt.scatter(x,y)
+#plt.show()
+
+xRandom = numpy.random.normal(5.0, 1.0, 1000)
+yRandom = numpy.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(xRandom, yRandom)
+plt.show();
