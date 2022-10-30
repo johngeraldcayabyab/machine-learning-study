01f6165281d5496f0446432535b0df691fc3a259
Dlareg <johngeraldcayabyab@gmail.com>
2022-10-30 12:18:43 +0800

    format files

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy
 
-car = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
-speed = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+car = [1, 2, 3, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 18, 19, 21, 22]
+speed = [100, 90, 80, 60, 60, 55, 60, 65, 70, 70, 75, 76, 78, 79, 90, 99, 99, 100]
 
+mymodel = numpy.poly1d(numpy.polyfit(car, speed, 3))
 
+myline = numpy.linspace(1, 22, 100)
 
-mymodel = numpy.poly1d(numpy.polyfit(car,speed,3))
-
-myline = numpy.linspace(1,22,100)
-
-plt.scatter(car,speed)
+plt.scatter(car, speed)
 plt.plot(myline, mymodel(myline))
 plt.show()
