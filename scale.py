f44d4c44cd16cdf239dbb2b071f6993c7e360553
Dlareg <johngeraldcayabyab@gmail.com>
2022-10-30 21:46:00 -0400

    scale and predict value

--- scale.py
+++ scale.py
@@ -3,10 +3,22 @@ from sklearn import linear_model
 from sklear.preprocessing import StandardScaler
 scale = StandardScaler()
 
-df = pandas.read_csv('./csv/data.csv')
+df = pandas.read_csv('./csv/data_scale.csv')
 
 X = df[['Weight', 'Volume']]
+y = df['C02']
 
 scaledX = scale.fit_transform(X)
 
+regr = linear_model.LinearRegression()
+regr.fit(scaledX,y)
+
 print(scaledX)
+
+scaled = scale.transform([[2300,1.3]])
+
+predictedC02 = regr.predict([[scaled[0]])
+                             
+print(predictedC02)
+
+
