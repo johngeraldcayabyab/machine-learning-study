5415a96633a97e7c04fb885a368c9d9094d34cf5
Dlareg <johngeraldcayabyab@gmail.com>
2022-11-07 16:01:31 +0800

    understanding how to plot MA

---
+++ stock/plotting_moving_average.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# window = 2
+# average_data = []
+#
+# for index in range(len(data) - window + 1):
+#     windowRange = data[index:index + window]  # list slice
+#     rangeMean = np.mean(windowRange)
+#     average_data.append(rangeMean)
+
+x = np.linspace(0, 10, 50)
+y = np.sin(x)
+window = 5
+average_y = []
+# for ind in range(len(y) - window)
