fbb07056ebd32b2a38af0ad11f1b39018b1a9735
Dlareg <johngeraldcayabyab@gmail.com>
2022-11-08 08:59:32 +0800

    need to revisit trigo again

--- stock/plotting_moving_average.py
+++ stock/plotting_moving_average.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # window = 2
@@ -9,16 +10,9 @@ import numpy as np
 #     rangeMean = np.mean(windowRange)
 #     average_data.append(rangeMean)
 
-# x = np.linspace(0, 10, 50)
-
-# print(x)
-
-# y = np.sin(x)
-# window = 5
-# average_y = []
-
-# len()
-# for ind in range(len(y) - window)
-
-
-print(np.linspace(1, 50, 100))
+x = np.linspace(0, 10, 50)
+y = np.sin(x)
+window = 5
+average_y = []
+for ind in range(len(y) - window + 1):
+    average_y.append(np.mean(y[ind:ind + window]))
