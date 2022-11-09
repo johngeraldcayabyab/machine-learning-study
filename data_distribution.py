0c912dda35b1b4bdbdc30f827221f1e88c2384ea
Dlareg <johngeraldcayabyab@gmail.com>
2022-11-09 14:05:40 +0800

    data distribution is all about plotting

--- data_distribution.py
+++ data_distribution.py
@@ -3,5 +3,8 @@ import matplotlib.pyplot as plt
 
 x = numpy.random.uniform(0.0, 5.0, 250)
 
+# uniform, give random number between 0.0 and 5.0. Third param gives you more sample size
+print(numpy.random.uniform(0.0, 5.0, 10))
+
 plt.hist(x, 5)
 plt.show()
