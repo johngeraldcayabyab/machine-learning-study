5b3d9252d905c812d34ff33d7583823badc35613
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2022-11-14 17:09:45 +0800

    and this is where i need to learn trigo again

--- train_test.py
+++ train_test.py
@@ -6,7 +6,15 @@ numpy.random.seed(2)
 x = numpy.random.normal(3, 1, 100)
 y = numpy.random.normal(150, 40, 100) / x
 
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
 # print(y)
 
-plt.scatter(x, y)
+plt.scatter(test_x, test_y)
 plt.show()
+
+
