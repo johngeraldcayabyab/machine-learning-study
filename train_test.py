c7497548682cf08249d2d129bc0ede2baec7eff6
Dlareg <johngeraldcayabyab@gmail.com>
2022-11-15 07:53:59 +0800

    i understand how the code works but not how the formula behind the scene works

--- train_test.py
+++ train_test.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 
 numpy.random.seed(2)
 
@@ -12,9 +13,17 @@ train_y = y[:80]
 test_x = x[80:]
 test_y = y[80:]
 
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+r2 = r2_score(test_y, mymodel(test_x))
+
+
+
+print(r2)
+
+print(mymodel(5))
+
 # print(y)
 
-plt.scatter(test_x, test_y)
-plt.show()
-
-
+# plt.scatter(test_x, test_y)
+# plt.show()
