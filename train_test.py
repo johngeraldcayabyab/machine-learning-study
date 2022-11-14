de5dc2f3cda01a355719925244bf8d943ec0fb61
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2022-11-14 17:02:54 +0800

    didnt know python can divide matrices automatically

--- train_test.py
+++ train_test.py
@@ -1,1 +1,12 @@
 import numpy
+import matplotlib.pyplot as plt
+
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+# print(y)
+
+plt.scatter(x, y)
+plt.show()
