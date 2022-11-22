e22d65d78245497ab39c8004a59d5b393365d2e6
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2022-11-22 22:08:12 +0800

    added comment

--- categorical_data.py
+++ categorical_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 # whats the difference of categorial data to scaling?
 # Problem with label encoding is that it assumes higher the categorical value, better the category. “Wait, What!?”.
 # This is why we use one hot encoder to perform “binarization” of the category and include it as a feature to train the model.
+#source of one hot encoding https://hackernoon.com/what-is-one-hot-encoding-why-and-when-do-you-have-to-use-it-e3c6186d008f
 
 cars = pd.read_csv('./csv/data.csv')
 # print(cars.to_string())
