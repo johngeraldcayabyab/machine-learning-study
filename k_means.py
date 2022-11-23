324a906d40c582321b3cb140c329c68df1aa7fd8
Dlareg <johngeraldcayabyab@gmail.com>
2022-11-23 14:10:58 +0800

    understanding k cluster

--- k_means.py
+++ k_means.py
@@ -14,16 +14,30 @@ y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
 # plt.show()
 
 data = list(zip(x, y))
-inertias = []
 
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters=i)
-    kmeans.fit(data)
-    inertias.append(kmeans.inertia_)
 
-plt.plot(range(1, 11), inertias, marker='o')
-plt.title('Elbow method')
-plt.xlabel('Number of clusters')
-plt.ylabel('Inertia')
+def elbow(theData):
+    inertias = []
+
+    for i in range(1, 11):
+        kmeans = KMeans(n_clusters=i)
+        kmeans.fit(theData)
+        inertias.append(kmeans.inertia_)
+
+    plt.plot(range(1, 11), inertias, marker='o')
+    plt.title('Elbow method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Inertia')
+    plt.show()
+
+
+# after looking at the visuals, the elbow point might look like 2
+# so fit the data
+
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(data)
+
+plt.scatter(x, y, c=kmeans.labels_)
 plt.show()
-print(data)
+
+# just like that, things got clustered
