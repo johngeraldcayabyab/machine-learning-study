ab91d84432cef19aec918eb999255235671a209e
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2022-11-28 22:38:53 +0800

    reset author removed

--- bin/git-reset-author.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch -f --env-filter "
-    GIT_AUTHOR_NAME='Dlareg'
-    GIT_AUTHOR_EMAIL='johngeraldcayabyab@gmail.com'
-    GIT_COMMITTER_NAME='Dlareg'
-    GIT_COMMITTER_EMAIL='johngeraldcayabyab@gmail.com'
-  " HEAD
