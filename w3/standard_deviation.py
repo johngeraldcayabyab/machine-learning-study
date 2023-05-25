0c056dc469f25e0ade85f7eaea0bbffec846a799
Dlareg <johngeraldcayabyab@gmail.com>
2023-05-25 14:48:59 +0800

    testing out variance method

--- w3/standard_deviation.py
+++ w3/standard_deviation.py
@@ -11,3 +11,10 @@ print(numpy.std([86, 87, 88, 86, 87, 85, 86]))
 
 # Standard Deviation is often represented by the symbol Sigma: σ
 # Variance is often represented by the symbol Sigma Squared: σ2
+
+
+print(numpy.var([86, 87, 88, 86, 87, 85, 86]))
+
+print(numpy.var([32, 111, 138, 28, 59, 77, 97]))
+
+print(numpy.var([86, 87, 88, 86, 87, 85, 86]))
