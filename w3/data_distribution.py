17e246e1770b1168c596cadf9dd0d035a813b4f2
Dlareg <johngeraldcayabyab@gmail.com>
2023-05-25 14:58:51 +0800

    refresher distribution

--- w3/data_distribution.py
+++ w3/data_distribution.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 x = numpy.random.uniform(0.0, 5.0, 250)
 
 # uniform, give random number between 0.0 and 5.0. Third param gives you more sample size
-print(numpy.random.uniform(0.0, 5.0, 10))
+print(x)
 
 plt.hist(x, 5)
 plt.show()
