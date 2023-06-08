89037e9ccc0ed8cc14b6657913e65f53b46bd761
Dlareg <johngeraldcayabyab@gmail.com>
2023-06-08 19:56:33 +0800

    trying to rotate cube, not working

--- test_gpu.py
+++ test_gpu.py
@@ -1,9 +1,16 @@
 import pygfx as gfx
+import pylinalg as la
 
 cube = gfx.Mesh(
     gfx.box_geometry(200, 200, 200),
     gfx.MeshPhongMaterial(color="#336699"),
 )
 
+rot = la.quat_from_euler((0, 0.01), order="XYZ")
+print(rot)
+
+def animate():
+    cube.rotation = la.quat_mul(rot, cube.rotation)
+
 if __name__ == "__main__":
-    gfx.show(cube)
+    gfx.show(cube, before_render=animate)
