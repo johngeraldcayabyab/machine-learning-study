e63bfe42aaf8b85711e9d44aaf6bf3111f66e889
Dlareg <johngeraldcayabyab@gmail.com>
2023-06-14 19:01:53 +0800

    test different animation

--- test_gpu.py
+++ test_gpu.py
@@ -1,16 +1,15 @@
 import pygfx as gfx
-import pylinalg as la
 
 cube = gfx.Mesh(
     gfx.box_geometry(200, 200, 200),
     gfx.MeshPhongMaterial(color="#336699"),
 )
 
-rot = la.quat_from_euler((0, 0.01), order="XYZ")
-print(rot)
 
 def animate():
-    cube.rotation = la.quat_mul(rot, cube.rotation)
+    rot = gfx.linalg.Quaternion().set_from_euler(gfx.linalg.Euler(0.005, 0.1))
+    cube.rotation.multiply(rot)
 
-if __name__ == "__main__":
-    gfx.show(cube, before_render=animate)
+disp = gfx.Display()
+disp.before_render = animate
+disp.show(cube)
