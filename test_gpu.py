297b0560f7e0af930704fc636028f888cccc95f5
Gerald <johngeraldcayabyab@gmail.com>
2023-07-16 04:32:32 +0800

    playing with geometry

--- test_gpu.py
+++ test_gpu.py
@@ -3,14 +3,17 @@ import pygfx as gfx
 import pylinalg as la
 
 cube = gfx.Mesh(
-    gfx.box_geometry(50, 50, 50),
-    gfx.MeshPhongMaterial(color="#336699"),
+    # gfx.box_geometry(50, 50, 50),
+
     # gfx.po
+    material=gfx.MeshPhongMaterial(color="#336699"),
+    geometry=gfx.Geometry(
+        positions=[50, 50, 50, 1, 1, 1]
+    )
 )
 
 pygfx.Controller.enabled = 1
 
-cube.local.position = (0, 300 ,0)
 
 # print(cube.positions)
 
@@ -21,6 +24,9 @@ def animate():
     rot = la.quat_from_euler((0.005, 0.01), order="XY")
     cube.local.rotation = la.quat_mul(rot, cube.local.rotation)
 
+
+cube.local.position = (0, 300, 0)
+print(cube)
 
 if __name__ == "__main__":
     disp = gfx.Display()
